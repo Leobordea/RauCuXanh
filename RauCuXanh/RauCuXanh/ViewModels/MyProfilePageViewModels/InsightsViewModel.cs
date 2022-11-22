83017a77ae62d44578df1d28783040353c12ccbc
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-11-22 19:34:16 +0700

    small ui update

--- RauCuXanh/RauCuXanh/ViewModels/MyProfilePageViewModels/InsightsViewModel.cs
+++ RauCuXanh/RauCuXanh/ViewModels/MyProfilePageViewModels/InsightsViewModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace RauCuXanh.ViewModels.MyProfilePageViewModels
+namespace RauCuXanh.ViewModels
 {
     public class InsightsViewModel : BaseViewModel
     {
