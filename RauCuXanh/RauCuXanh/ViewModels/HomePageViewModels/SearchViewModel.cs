96c8cf8523e868681b4ff188a5d518bdde0ab898
Lê Quang Thắng <lthang052@gmail.com>
2022-11-28 17:26:12 +0700

    Add search bar and search page

---
+++ RauCuXanh/RauCuXanh/ViewModels/HomePageViewModels/SearchViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RauCuXanh.ViewModels.HomePageViewModels
+{
+    public class SearchViewModel : HomeViewModel
+    {
+        private string searchText;
+        public string SearchText
+        {
+            get { return searchText; }
+            set { SetProperty(ref searchText, value); }
+        }
+
+        public SearchViewModel() { }
+        public SearchViewModel(string s)
+        {
+            Title = "Tìm kiếm";
+            SearchText = s;
+        }
+    }
+}
--- RauCuXanh/RauCuXanh/Views/HomePageViews/HomePage.xaml.cs
+++ RauCuXanh/RauCuXanh/Views/HomePageViews/HomePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using RauCuXanh.ViewModels.HomePageViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,26 @@ namespace RauCuXanh.Views.HomePageViews
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var _container = BindingContext as HomeViewModel;
+            SearchSuggestion.BeginRefresh();
+
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+            {
+                SearchSuggestion.ItemsSource = null;
+                SearchSuggestion.IsVisible = false;
+            }
+            else
+            {
+                var itemsource = _container.Products.Where(i => i.Name.ToLower().Contains(e.NewTextValue.ToLower()));
+                SearchSuggestion.ItemsSource = itemsource;
+                SearchSuggestion.HeightRequest = Math.Min(60 * itemsource.Count(), 240);
+                SearchSuggestion.IsVisible = true;
+            }
+            SearchSuggestion.EndRefresh();
         }
     }
 }
